3f3fc6acc6d9a8c37ae06fe0ed2003c94bafc863
clemence <clem.bernard40@gmail.com>
2023-04-19 11:51:36 +0900

    made the desktop version of the website so that it's fully responsive

--- DRIZONE_WEBSITE/javascript.js
+++ DRIZONE_WEBSITE/javascript.js
@@ -12,3 +12,11 @@ const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
 mobileMenuBtn.addEventListener('click', function() {
   this.classList.toggle('open');
 });
+function zoom(e){
+  var zoomer = e.currentTarget;
+  e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+  e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+  x = offsetX/zoomer.offsetWidth*100
+  y = offsetY/zoomer.offsetHeight*100
+  zoomer.style.backgroundPosition = x + '% ' + y + '%';
+}
